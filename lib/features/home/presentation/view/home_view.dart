e456642a0f09844171a389b6ed9fde2496ec3393
KhaledElalfy1 <khaledalfy53@gmail.com>
2024-10-28 21:34:20 +0300

    add main body for screens

--- lib/features/home/presentation/view/home_view.dart
+++ lib/features/home/presentation/view/home_view.dart
@@ -1,14 +1,74 @@
+import 'package:ajuda/core/utils/app_colors.dart';
+import 'package:ajuda/core/utils/app_icons.dart';
+import 'package:ajuda/core/widgets/custom_text_form_filed.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/svg.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Home View'),
-      ),
-    );
+    return Scaffold(
+        body: Stack(
+      children: [
+        Container(
+          padding: EdgeInsets.symmetric(horizontal: 16.h, vertical: 40.h),
+          height: MediaQuery.sizeOf(context).height,
+          width: MediaQuery.sizeOf(context).width,
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [
+                AppColors.gradientTwo,
+                AppColors.gradientOne,
+              ],
+            ),
+          ),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Expanded(
+                child: CustomTextFormFiled(
+                  controller: TextEditingController(),
+                  hintText: 'Search',
+                  keyboardType: TextInputType.text,
+                  icon: IconButton(
+                    onPressed: () {},
+                    icon: SvgPicture.asset(
+                      AppIcons.iconsSearch,
+                      height: 16.h,
+                      width: 16.h,
+                    ),
+                  ),
+                ),
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: SvgPicture.asset(
+                  AppIcons.iconsSaved,
+                ),
+              ),
+            ],
+          ),
+        ),
+        Positioned(
+          top: 120.h,
+          child: Container(
+            height: MediaQuery.sizeOf(context).height - 120.h,
+            width: MediaQuery.sizeOf(context).width,
+            decoration: BoxDecoration(
+              color: AppColors.white,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(30.r),
+                topRight: Radius.circular(30.r),
+              ),
+            ),
+          ),
+        )
+      ],
+    ));
   }
 }
