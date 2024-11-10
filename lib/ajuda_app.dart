61b21e024d35ab9759fed991245067e448d4ab18
KhaledElalfy1 <khaledalfy53@gmail.com>
2024-11-10 20:29:38 +0200

    add localization for android

--- lib/ajuda_app.dart
+++ lib/ajuda_app.dart
@@ -1,4 +1,3 @@
-
 import 'package:ajuda/core/database/cache/cashe_helper.dart';
 import 'package:ajuda/core/routers/app_routers.dart';
 import 'package:ajuda/core/routers/routing.dart';
@@ -6,6 +5,8 @@ import 'package:ajuda/core/themes/themes.dart';
 import 'package:ajuda/core/services/get_it.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'generated/l10n.dart';
 
 class AjudaApp extends StatelessWidget {
   const AjudaApp({super.key});
@@ -13,17 +14,24 @@ class AjudaApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
-      designSize:   const Size(375, 812),
-      builder: (_ , child) {
-        return MaterialApp(
-          debugShowCheckedModeBanner: false,
-          initialRoute:initRouting() ,
-          title: 'Ajuda',
-          theme: appTheme(),
-          onGenerateRoute: AppRouters().generateRoute,
-        );
-      }
-    );
+        designSize: const Size(375, 812),
+        builder: (_, child) {
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            localizationsDelegates: const [
+              S.delegate,
+              GlobalMaterialLocalizations.delegate,
+              GlobalWidgetsLocalizations.delegate,
+              GlobalCupertinoLocalizations.delegate,
+            ],
+            supportedLocales: S.delegate.supportedLocales,
+            locale: const Locale('en'),
+            initialRoute: initRouting(),
+            title: 'Ajuda',
+            theme: appTheme(),
+            onGenerateRoute: AppRouters().generateRoute,
+          );
+        });
   }
 
   String initRouting() {
