437fa7e070c9b397aac2680f133d88b20ef3dddb
KhaledElalfy1 <khaledalfy53@gmail.com>
2024-12-11 20:22:56 +0200

    provide dio injection and api, dio consumer injection

--- lib/core/services/get_it.dart
+++ lib/core/services/get_it.dart
@@ -1,5 +1,8 @@
+import 'package:ajuda/core/database/api/api_consumer.dart';
+import 'package:ajuda/core/database/api/dio_consumer.dart';
 import 'package:ajuda/core/database/cache/cashe_helper.dart';
 import 'package:ajuda/features/reset_password/presentation/view_model/forget_password_cubit/forget_password_cubit.dart';
+import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
 
 final getIt = GetIt.instance;
@@ -7,7 +10,13 @@ final getIt = GetIt.instance;
 void setup() {
   getIt.registerSingleton<CacheHelper>(CacheHelper());
 
+  getIt.registerSingleton<Dio>(Dio());
+
+  getIt.registerSingleton<ApiConsumer>(
+    DioConsumer(
+      dio: getIt<Dio>(),
+    ),
+  );
+
   getIt.registerSingleton<ForgetPasswordCubit>(ForgetPasswordCubit());
-
-
 }
