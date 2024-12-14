42695785f944b06fff4d1b7dc246a97fb3995e16
KhaledElalfy1 <khaledalfy53@gmail.com>
2024-12-14 07:45:12 +0200

    use secure storage for token and id caching

--- lib/core/database/cache/cache_keys.dart
+++ lib/core/database/cache/cache_keys.dart
@@ -2,4 +2,6 @@ abstract class CacheKeys {
   static const String isFirstTime = 'isFirstTime';
   static const String currentLanguageCode = 'currentLanguageCode';
   static const String currentLanguage = 'currentLanguage';
+  static const String token = "token";
+  static const String id = "id";
 }
--- lib/features/auth/data/repo/auth_repo_impl.dart
+++ lib/features/auth/data/repo/auth_repo_impl.dart
@@ -2,10 +2,13 @@ import 'dart:developer';
 
 import 'package:ajuda/core/database/api/api_consumer.dart';
 import 'package:ajuda/core/database/api/api_keys.dart';
+import 'package:ajuda/core/database/cache/cache_keys.dart';
+import 'package:ajuda/core/database/cache/secure_storage.dart';
 import 'package:ajuda/features/auth/data/repo/auth_repo.dart';
 import 'package:ajuda/features/auth/model/auth_model.dart';
 import 'package:ajuda/features/auth/model/sign_in_user_input_model.dart';
 import 'package:dartz/dartz.dart';
+import 'package:jwt_decode/jwt_decode.dart';
 
 class AuthRepoImpl extends AuthRepo {
   final ApiConsumer apiConsumer;
@@ -19,6 +22,12 @@ class AuthRepoImpl extends AuthRepo {
           body: signInUserInputModel.toJson());
       final data = AuthModel.fromJson(response.data);
       log("the response from sign in repo impl is $data");
+      final token = data.token;
+      Map<String, dynamic> payload = Jwt.parseJwt(token);
+
+      await SecureStorage.instance.addData(key: CacheKeys.token, data: token);
+      await SecureStorage.instance
+          .addData(key: CacheKeys.id, data: payload['sub']);
       return right(data.userName);
     } catch (e) {
       log("error from sign in repo impl is $e");
