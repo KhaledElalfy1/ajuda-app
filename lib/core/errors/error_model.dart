7eee27614b86cfcb575ee430a2e8ddc5d63b70b6
KhaledElalfy1 <khaledalfy53@gmail.com>
2024-12-14 07:06:00 +0200

    edit error model

--- lib/core/errors/error_model.dart
+++ lib/core/errors/error_model.dart
@@ -1,9 +1,13 @@
 class ErrorModel {
   final String message;
+  final bool isAuthenticated;
 
-  ErrorModel({required this.message});
+  ErrorModel({required this.message, required this.isAuthenticated});
 
   factory ErrorModel.fromJson(Map<String, dynamic> jsonData) {
-    return ErrorModel(message: jsonData['message']);
+    return ErrorModel(
+      message: jsonData['message'],
+      isAuthenticated: jsonData['isAuthenticated'],
+    );
   }
 }
